34046ee6033ad0403e4a65225804dbb0087c5e57
Ganjali717 <ganjaliki@code.edu.az>
2021-10-07 17:01:54 +0400

    First we are creating our DbContext and put template in our project

---
+++ Quarter/Quarter/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Quarter.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quarter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Quarter/Quarter/Models/Slider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quarter.Models
+{
+    public class Slider
+    {
+    }
+}
